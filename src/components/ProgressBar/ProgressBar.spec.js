f9aaefc6baf185bc74c624f50ac485acc6f3c208
Ivan <belzee92@gmail.com>
2019-10-08 21:10:04 -0400

    fix ProgressBar tests

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -11,22 +11,19 @@ describe('ProgressBar', () => {
     vuetify = new Vuetify();
   });
 
-  const mountFunction = options => {
-    return mount(ProgressBar, {
-      localVue,
-      vuetify,
-      ...options
-    });
-  };
-
   test('should hidden on initial render', () => {
-    const wrapper = mountFunction();
+    const wrapper = mount(ProgressBar, {
+      localVue,
+      vuetify
+    });
     const progressBar = wrapper.find('.v-progress-linear');
     expect(progressBar.element.style.height).toBe('0px');
   });
 
-  test('should be visible when the prop active changes', () => {
-    const wrapper = mountFunction({
+  test('should be visible when the prop active is true', () => {
+    const wrapper = mount(ProgressBar, {
+      localVue,
+      vuetify,
       propsData: { active: true }
     });
     const progressBar = wrapper.find('.v-progress-linear');
